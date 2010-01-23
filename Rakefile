74f5e0688d00194002bc0cfef5a0830230b6daac
Roja Buck <roja@arbia.co.uk>
2010-01-23 19:53:01 +0000

    Very basic information only

--- Rakefile
+++ Rakefile
@@ -5,15 +5,19 @@ begin
   require 'jeweler'
   Jeweler::Tasks.new do |gem|
     gem.name = "system"
-    gem.summary = %Q{TODO: one-line summary of your gem}
-    gem.description = %Q{TODO: longer description of your gem}
+    gem.summary = %Q{System is a pure ruby interface to gather current systems information; OS, CPU, Filesystem etc.}
+    gem.description = %Q{System is a pure ruby interface to gather systems information from the current host. System offers a simple to use interface to gather an array of information including; OS, CPU, Filesystem etc.}
     gem.email = "roja@arbia.co.uk"
     gem.homepage = "http://github.com/roja/system"
     gem.authors = ["Roja Buck"]
-    gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
+    gem.rubyforge_project = 'system'
+    #    gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
     # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
   end
   Jeweler::GemcutterTasks.new
+  Jeweler::RubyforgeTasks.new do |rubyforge|
+    rubyforge.doc_task = "rdoc"
+  end
 rescue LoadError
   puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
 end
---
+++ lib/system.rb
@@ -0,0 +1,56 @@
+# system includes
+require 'singleton'
+
+# java includes
+if /java/.match(RUBY_PLATFORM)
+  require 'java'
+  import 'java.lang.System'
+end
+
+module HostSystem
+
+  def self.os
+    SystemInfo.instance.os
+  end
+
+  def self.jruby?
+    !(/java/.match(RUBY_PLATFORM).nil?)
+  end
+
+  class SystemInfo
+    include Singleton
+
+    def initialize
+    end
+    
+    def os
+      return @os if defined? @os # if we already know then pass it
+      if HostSystem::jruby?
+        @os = case System.getProperty('os.name').downcase
+        when /linux/ then :linux
+        when /win/ then :windows
+        when /solaris/ then :solaris
+        when /bsd/ then :basd
+        when /darwin/ then :darwin
+        when /mac.*?os.*?x/ then :osx
+        else
+          :unknown
+        end
+      else
+        potential_os = case RUBY_PLATFORM
+        when /linux/ then :linux
+        when /win/ then :windows
+        when /solaris/ then :solaris
+        when /bsd/ then :basd
+        when /darwin/ then :darwin
+        else
+          :unknown
+        end
+        potential_os = :osx if potential_os == :darwin && defined? Config::CONFIG && Config::CONFIG['build_vendor'] == 'apple'
+        @os = potential_os
+      end      
+    end
+
+  end
+
+end
