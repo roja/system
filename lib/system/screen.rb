5224b1450ad7b9a02938ee45d8bb876ee96a8827
Ryan Scott Lewis <ryan@rynet.us>
2012-11-23 14:25:48 -0500

    System::Screen: .all, .primary, .resolution, .width, .height

--- lib/system/screen.rb
+++ lib/system/screen.rb
@@ -10,20 +10,67 @@ class System
     
     class << self
       
-      # The user's primary Screen.
+      # All Screen instances attached to the current host.
+      # 
+      # @return [<Screen>]
+      # @since 0.1.4
+      def all
+        
+      end
+      
+      # The current host's primary Screen.
       # 
       # @return [Screen]
       # @since 0.1.4
       def primary
+        screen = new
         
+        x, y = [nil, nil]
+        
+        if OS.windows?
+          # SEE: http://stackoverflow.com/a/4631379/139089
+          
+          require 'dl/import'
+          require 'dl/struct'
+          
+          user32 = DL.dlopen("user32")
+          
+          get_system_metrics = user32['GetSystemMetrics', 'ILI']
+          x, tmp = get_system_metrics.call(0, 0)
+          y, tmp = get_system_metrics.call(1, 0)
+          
+          [x, y]
+        elsif OS.unix?
+          `xrandr`.scan(/current (\d+) x (\d+)/).flatten
+        end
+        
+        screen.resolution = Resolution.new(x: x, y: y)
+        
+        screen
       end
       
-      # The user's primary Screen's resolution.
+      # The current host's primary Screen's resolution.
       # 
       # @return [Screen::Resolution]
       # @since 0.1.4
       def resolution
-        
+        primary.resolution
+      end
+      
+      # The current host's primary Screen's resolution's width.
+      # 
+      # @return [Integer]
+      # @since 0.1.4
+      def width
+        resolution.width
+      end
+      
+      # The current host's primary Screen's resolution's height.
+      # 
+      # @return [Integer]
+      # @since 0.1.4
+      def height
+        resolution.height
       end
       
     end
